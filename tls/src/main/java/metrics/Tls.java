cf46b805ce2ca5c1686ee315ab6bb6f48abfeeca
André Meneses <andre_meneses2001@hotmail.com>
2023-09-29 13:05:37 -0400

    Add initial version of tloc

--- tls/src/main/java/metrics/Tls.java
+++ tls/src/main/java/metrics/Tls.java
@@ -1,9 +1,33 @@
 package metrics;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tls {
 
     public static void main(String[] args){
-        System.out.println("Testando maven package");
+
+        if (args.length != 1) {
+            System.out.println("You must provide one folder path");
+            return;
+        }
+
+        String dossier = args[0];
+        List<String> lignesCSV = new ArrayList<>();
+
+        lignesCSV.add("Chemin du fichier,Nom du paquet,Nom de la classe");
+
+        DirScanner.traverseFolder(dossier, lignesCSV);
+
+        try {
+            Files.write(Paths.get("resultat.csv"), lignesCSV);
+            System.out.println("Le fichier CSV 'resultat.csv' a été généré avec succès.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
