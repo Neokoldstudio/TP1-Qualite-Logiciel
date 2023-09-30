c70251449b9899088af2e37e95b97b1b42399816
André Meneses <andre_meneses2001@hotmail.com>
2023-09-29 20:35:45 -0400

    Fix identation

--- tls/src/main/java/metrics/Parser.java
+++ tls/src/main/java/metrics/Parser.java
@@ -8,23 +8,22 @@ public class Parser {
     
     public static void extractInfo(File fichier, List<String> lignesCSV) {
 
-    String cheminFichier = fichier.getAbsolutePath();
-    System.out.println(cheminFichier);
-    String nomFichier = fichier.getPath(); // Get the file name
-    String nomPaquet = "";
-    String nomClasse = "";
+        String cheminFichier = fichier.getAbsolutePath();
+        String nomFichier = fichier.getPath(); // Get the file name
+        String nomPaquet = "";
+        String nomClasse = "";
 
-    // Extract package and class name from the file path
-    String[] pathSegments = cheminFichier.split("/src/test/java/");
-    if (pathSegments.length == 2) {
-        String[] packageAndClass = pathSegments[1].split("/");
-        if (packageAndClass.length >= 2) {
-            nomPaquet = String.join(".", Arrays.copyOf(packageAndClass, packageAndClass.length - 1));
-            nomClasse = packageAndClass[packageAndClass.length - 1].replaceAll(".java$", "");
+        // Extract package and class name from the file path
+        String[] pathSegments = cheminFichier.split("/src/test/java/");
+        if (pathSegments.length == 2) {
+            String[] packageAndClass = pathSegments[1].split("/");
+            if (packageAndClass.length >= 2) {
+                nomPaquet = String.join(".", Arrays.copyOf(packageAndClass, packageAndClass.length - 1));
+                nomClasse = packageAndClass[packageAndClass.length - 1].replaceAll(".java$", "");
+            }
         }
-    }
 
-    lignesCSV.add(nomFichier + "," + nomPaquet + "," + nomClasse);
+        lignesCSV.add(nomFichier + "," + nomPaquet + "," + nomClasse);
     }
         
 }
