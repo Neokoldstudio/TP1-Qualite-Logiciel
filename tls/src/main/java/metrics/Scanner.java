638b3a3debf9f50342072262e9a7f83d33dffc20
André Meneses <andre_meneses2001@hotmail.com>
2023-10-01 19:06:41 -0400

    Add tloc metric, rename class and more comments for enhanced clarity

---
+++ tls/src/main/java/metrics/Scanner.java
@@ -0,0 +1,24 @@
+package metrics;
+
+import java.io.File;
+import java.util.List;
+
+public class Scanner {
+
+    public static void traverseFolder(String folder, List<String> linesCSV) {
+
+        File currentFolder = new File(folder);
+        File[] files = currentFolder.listFiles();
+
+        // If there is another folder, keep traversing, if not extract file's information
+        if (files != null) {
+            for (File fichier : files) {
+                if (fichier.isDirectory()) {
+                    traverseFolder(fichier.getPath(), linesCSV);
+                } else if (fichier.isFile() && fichier.getName().endsWith(".java")) {
+                    Extractor.extractInfo(fichier, linesCSV);
+                }
+            }
+        }
+    }
+}
