0f02678a3869cc9b745c4ecf4fd92860c7c9458d
Neokoldstudio <nightmare.vid@gmail.com>
2023-10-03 09:56:14 -0400

    setup tropcomp project

---
+++ tropcomp/src/main/java/metrics/Tropcomp.java
@@ -0,0 +1,11 @@
+package metrics;
+
+public class Tropcomp {
+
+    // TODO : Usage | main | args gestion
+    // TODO : tropcomp : apply tls for all files in the project (rec search ?)
+    public static void main(String[] args) {
+        // WIP :)
+
+    }
+}
