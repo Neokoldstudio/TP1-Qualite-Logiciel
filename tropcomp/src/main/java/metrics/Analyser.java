caddda767adc364af01c9e4c4671692ce059007e
André Meneses <andre_meneses2001@hotmail.com>
2023-10-03 23:56:45 -0400

    Modify tropcomp/pom.xml. - Add tls dependency

--- tropcomp/src/main/java/metrics/Analyser.java
+++ tropcomp/src/main/java/metrics/Analyser.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class Analyser {
     public static void analyseProject(String folder, int threshold, List<String> linesCSV) {
         List<String> scannedFiles = new ArrayList<>();
-        // Scanner.traverseFolder(folder, scannedFiles); -> find a way to use Scanner
+        Scanner.traverseFolder(folder, scannedFiles); 
     }
 }
